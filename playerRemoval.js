6cc3ca309116feddafaef2460a804c68702fcbb3
R-Slezak <rob.slezak@live.com>
2022-06-05 02:07:07 -0400

    Adding additional check to add the css classes

--- playerRemoval.js
+++ playerRemoval.js
@@ -1,4 +1,5 @@
 var playerDeleted = false;
+var shiftContent = false;
 var videoInterval = null;
 var oldHref = document.location.href;
 
@@ -26,16 +27,17 @@ function checkVideo() {
 // Handles tweaking the css for the page and adding/removing the interval to delete the video player DOM Node based on the url for the page
 function removeVideoPlayer() {
 	var match = document.location.href.match(new RegExp("twitch.tv/.*/(videos|about|schedule)"));
-
+	
 	var rootContent = document.getElementsByClassName("channel-root__info")[0];
 	var content = document.getElementsByClassName("channel-info-content")[0];
-	
+
 	if (match == null) {
 		if (playerDeleted) {
 			//trigger page reload to restore video player
 			location.reload();
 		}
 		else {
+			shiftContent = false;
 			rootContent.classList.remove("hideRootContent");
 			content.classList.remove("hideContent");
 			
@@ -47,6 +49,7 @@ function removeVideoPlayer() {
 		}
 	}
 	else {
+		shiftContent = true;
 		rootContent.classList.add("hideRootContent");
 		content.classList.add("hideContent");
 
@@ -64,6 +67,18 @@ var urlObserver = new MutationObserver(function(mutations) {
 			oldHref = document.location.href;
 			removeVideoPlayer();
 		}
+		
+		var rootContent = document.getElementsByClassName("channel-root__info")[0];
+		var content = document.getElementsByClassName("channel-info-content")[0];
+		if (shiftContent) {
+			rootContent.classList.add("hideRootContent");
+			content.classList.add("hideContent");
+		}
+		else {
+			rootContent.classList.remove("hideRootContent");
+			content.classList.remove("hideContent");
+		}
+
 	});
 });
 
